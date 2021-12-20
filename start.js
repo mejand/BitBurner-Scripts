cd2c10ff2998c0b709611053264551c1ce1ef9e8
mejand <andreas.mejstrik@gmx.at>
2021-12-20 22:40:18 +0100

    added simple start script

---
+++ start.js
@@ -0,0 +1,16 @@
+/**
+ * Download the startup script from github and run it.
+ * @param {import(".").NS } ns
+ */
+export async function main(ns) {
+    // download the file from github
+    var success = ns.wget("https://github.com/mejand/BitBurner-Scripts/blob/main/start_automation.js", "start_automation.js");
+
+    // inform the user about the success or failure and start the startup script
+    if (success) {
+        ns.tprint("#### Startup Script download successful ####");
+        ns.spawn("start_automation", 1);
+    } else {
+        ns.alert("#### Download of Startup Script failed ####");
+    }
+}
