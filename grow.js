e4f0edfa9d8cf0199c9c583625398f0411ab72f8
mejand <andreas.mejstrik@gmx.at>
2021-12-23 16:33:58 +0100

    delay script execution

--- grow.js
+++ grow.js
@@ -3,6 +3,16 @@
  * @param {import(".").NS } ns
  */
 export async function main(ns) {
-  var target = ns.args[0];
-  await ns.grow(target);
+  var targetName = "n00dles";
+  if (ns.args.length > 0 && typeof ns.args[0] == "string") {
+    targetName = ns.args[0];
+  }
+  var delay = 0;
+  if (ns.args.length > 1 && typeof ns.args[1] == "number") {
+    delay = ns.args[1];
+  }
+  if (delay > 0) {
+    await ns.sleep(delay);
+  }
+  await ns.grow(targetName);
 }
