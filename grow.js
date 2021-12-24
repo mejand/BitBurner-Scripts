7694a2858e45f3957e2fe8294a4f4312c72f32bd
mejand <andreas.mejstrik@gmx.at>
2021-12-24 13:01:47 +0100

    formatting improvements

--- grow.js
+++ grow.js
@@ -3,16 +3,26 @@
  * @param {import(".").NS } ns
  */
 export async function main(ns) {
+  /**
+   * The name of the target server.
+   * @type {string}
+   */
   var targetName = "n00dles";
   if (ns.args.length > 0 && typeof ns.args[0] == "string") {
     targetName = ns.args[0];
   }
+
+  /**
+   * The delay time before the operation starts.
+   */
   var delay = 0;
   if (ns.args.length > 1 && typeof ns.args[1] == "number") {
     delay = ns.args[1];
   }
+
   if (delay > 0) {
     await ns.sleep(delay);
   }
+
   await ns.grow(targetName);
 }
--- hack.js
+++ hack.js
@@ -1,18 +1,28 @@
 /**
- * Run a single hack operation.
+ * Run a single grow operation.
  * @param {import(".").NS } ns
  */
 export async function main(ns) {
+  /**
+   * The name of the target server.
+   * @type {string}
+   */
   var targetName = "n00dles";
   if (ns.args.length > 0 && typeof ns.args[0] == "string") {
     targetName = ns.args[0];
   }
+
+  /**
+   * The delay time before the operation starts.
+   */
   var delay = 0;
   if (ns.args.length > 1 && typeof ns.args[1] == "number") {
     delay = ns.args[1];
   }
+
   if (delay > 0) {
     await ns.sleep(delay);
   }
+
   await ns.hack(targetName);
 }
