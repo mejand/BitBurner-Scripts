47bae8f180c13c4269a02cb723e00ecce2ff59e2
mejand <andreas.mejstrik@gmx.at>
2021-12-25 16:00:14 +0100

    getWeakenThreads

--- local_batch_control.js
+++ local_batch_control.js
@@ -46,6 +46,23 @@ export async function main(ns) {
      */
     let hostServer = ns.getServer(hostName);
 
+    /**
+     * The number of threads needed to grow the target to max money.
+     * @type {number}
+     */
+    let growThreads = getGrowThreads(ns, targetServer, hostServer);
+
+    /**
+     * The number of threads needed to reduce the target security to minimum.
+     * @type {number}
+     */
+    let weakenThreads = getWeakenThreads(
+      ns,
+      targetServer,
+      hostServer,
+      growThreads
+    );
+
     await ns.sleep(10);
   }
 }
@@ -79,6 +96,40 @@ function getGrowThreads(ns, targetServer, hostServer) {
   count = Math.ceil(
     ns.growthAnalyze(hostServer.hostname, growFactor, hostServer.cpuCores)
   );
+
+  return count;
+}
+
+/**
+ *
+ * @param {import(".").NS} ns
+ * @param {import(".").Server} targetServer - The target server.
+ * @param {import(".").Server} hostServer - The host server.
+ * @param {number} growThreads - The number of threads dedicated growing to max money.
+ * @returns {number} The number of threads needed to grow the target to max money.
+ */
+function getWeakenThreads(ns, targetServer, hostServer, growThreads) {
+  /**
+   * The number of threads needed to grow the target to max money.
+   * @type {number}
+   */
+  var count = 0;
+
+  /**
+   * The security score that needs to be removed to reach min security.
+   * @type {number}
+   */
+  var deltaSecurity = targetServer.hackDifficulty - targetServer.minDifficulty;
+
+  deltaSecurity += ns.growthAnalyzeSecurity(growThreads);
+
+  /**
+   * The security score that will be removed by one thread of the weaken script.
+   * @type {number}
+   */
+  var weakenReduction = ns.weakenAnalyze(1, hostServer.cpuCores);
+
+  count = Math.ceil(deltaSecurity / weakenReduction);
 
   return count;
 }
