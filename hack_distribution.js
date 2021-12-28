bf6deeee4421a83b274eea0c3b5716149356ff1b
mejand <andreas.mejstrik@gmx.at>
2021-12-28 12:42:03 +0100

    Batch class

--- hack_distribution.js
+++ hack_distribution.js
@@ -356,3 +356,228 @@ export class BatchHandler {
     }
   }
 }
+
+/**
+ * A batch of hack, grow and weaken scripts with defined belay times.
+ */
+class Batch {
+  /**
+   * The amount of time in milliseconds that shall be maintained between batches.
+   * @type {number}
+   */
+  static offsetPadding = 10;
+
+  /**
+   * Create an instance of batch.
+   * @param {string} targetName - The name of the target server.
+   * @param {number} hackThreads - The number of threads dedicated to hacking.
+   * @param {number} growThreads - The number of threads dedicated to growing.
+   * @param {number} weakenThreads - The number of threads dedicated to weakening.
+   * @param {number} hackDelay - The time in milliseconds that the execution of the hack script shall be delayed.
+   * @param {number} growDelay - The time in milliseconds that the execution of the grow script shall be delayed.
+   * @param {number} weakenDelay - The time in milliseconds that the execution of the weaken script shall be delayed.
+   */
+  constructor(
+    targetName = "n00dles",
+    hackThreads = 0,
+    growThreads = 0,
+    weakenThreads = 0,
+    hackDelay = 0,
+    growDelay = 0,
+    weakenDelay = 0
+  ) {
+    /**
+     * The name of the target server.
+     * @type {string}
+     */
+    this.targetName = targetName;
+
+    /**
+     * The number of threads dedicated to hacking.
+     * @type {number}
+     */
+    this.hackThreads = hackThreads;
+
+    /**
+     * The number of threads dedicated to growing.
+     * @type {number}
+     */
+    this.growThreads = growThreads;
+
+    /**
+     * The number of threads dedicated to weakening.
+     * @type {number}
+     */
+    this.weakenThreads = weakenThreads;
+
+    /**
+     * The time in milliseconds that the execution of the hack script shall be delayed.
+     * @type {number}
+     */
+    this.hackDelay = hackDelay;
+
+    /**
+     * The time in milliseconds that the execution of the grow script shall be delayed.
+     * @type {number}
+     */
+    this.growDelay = growDelay;
+
+    /**
+     * The time in milliseconds that the execution of the weaken script shall be delayed.
+     * @type {number}
+     */
+    this.weakenDelay = weakenDelay;
+
+    /**
+     * The number of batches that shall be executed before this batch.
+     * @type {number}
+     */
+    this._offsetCount = 0;
+  }
+
+  /**
+   * @param {number} newCount - The number of batches that shall be executed before this batch.
+   */
+  set offsetCount(newCount) {
+    /**
+     * The actual offset time in milliseconds that shall be added to this batch
+     * to maintain the needed distance to the prior batch.
+     * @type {number}
+     */
+    let offsetActual = offsetCount * Batch.offsetPadding;
+
+    this.hackDelay += offsetActual;
+    this.growDelay += offsetActual;
+    this.weakenDelay += offsetActual;
+
+    this._offsetCount = newCount;
+  }
+
+  get offsetCount() {
+    return this._offsetCount;
+  }
+
+  /**
+   * There are still scripts that have to be started in this batch.
+   * @type {boolean}
+   */
+  get alive() {
+    return (
+      this.hackThreads > 0 && this.growThreads > 0 && this.weakenThreads > 0
+    );
+  }
+
+  /**
+   * Execute as many of the scripts in the batch as possible on the given server.
+   * @param {import(".").NS} ns
+   * @param {MyServer} server - The MyServer object on which the scripts shall be executed.
+   */
+  execute(ns, server) {
+    if (server.useable) {
+      /**
+       * The number of hack threads that can be started on this server.
+       * @type {number}
+       */
+      let hackThreadsToExecute = Math.min(
+        server.threadsAvailable,
+        this.hackThreads
+      );
+      server.threadsAvailable -= hackThreadsToExecute;
+
+      /**
+       * The number of grow threads that can be started on this server.
+       * @type {number}
+       */
+      let growThreadsToExecute = Math.min(
+        server.threadsAvailable,
+        this.growThreads
+      );
+      server.threadsAvailable -= growThreadsToExecute;
+
+      /**
+       * The number of weaken threads that can be started on this server.
+       * @type {number}
+       */
+      let weakenThreadsToExecute = Math.min(
+        server.threadsAvailable,
+        this.weakenThreads
+      );
+      server.threadsAvailable -= weakenThreadsToExecute;
+
+      if (hackThreadsToExecute > 0) {
+        ns.exec(
+          "hack.js",
+          server.name,
+          hackThreadsToExecute,
+          this.targetName,
+          this.hackDelay
+        );
+      }
+
+      if (growThreadsToExecute > 0) {
+        ns.exec(
+          "grow.js",
+          server.name,
+          growThreadsToExecute,
+          this.targetName,
+          this.growDelay
+        );
+      }
+
+      if (weakenThreadsToExecute > 0) {
+        ns.exec(
+          "weaken.js",
+          server.name,
+          weakenThreadsToExecute,
+          this.targetName,
+          this.weakenDelay
+        );
+      }
+    }
+  }
+}
+
+/**
+ * All data of a server relevant for batch execution.
+ */
+class MyServer {
+  /**
+   * Create an instance of the MyServer class.
+   * @param {import(".").NS} ns
+   * @param {string} name - The mane of the server.
+   * @param {number} ramPerScript - The amount of ram needed to run any script.
+   */
+  constructor(ns, name, ramPerScript) {
+    /**
+     * The name of the server.
+     * @type {string}
+     */
+    this.name = name;
+
+    /**
+     * The amount of ram available on the server (when this instance was created).
+     * @type {number}
+     */
+    this.ramAvailable = ns.getServerMaxRam(name) - ns.getServerUsedRam;
+
+    /**
+     * The maximum nunber of threads that can be run on the server.
+     * @type {number}
+     */
+    this.threadsMax = Math.floor(ns.getServerMaxRam / ramPerScript);
+
+    /**
+     * The amount of threads available (when this instance was created).
+     * @type {number}
+     */
+    this.threadsAvailable = Math.floor(this.ramAvailable / ramPerScript);
+  }
+
+  /**
+   * Does this server have threads available for tasking.
+   * @type {boolean}
+   */
+  get useable() {
+    return this.threadsAvailable > 0;
+  }
+}
