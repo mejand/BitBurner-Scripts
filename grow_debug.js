7c05c0edf1e4924572c90742b554d334d4947866
mejand <andreas.mejstrik@gmx.at>
2021-12-28 22:04:18 +0100

    grow debug script

---
+++ grow_debug.js
@@ -0,0 +1,65 @@
+/**
+ * Run a single grow operation and print the result for debugging.
+ * @param {import(".").NS } ns
+ */
+export async function main(ns) {
+  /**
+   * The name of the target server.
+   * @type {string}
+   */
+  var targetName = "n00dles";
+  if (ns.args.length > 0 && typeof ns.args[0] == "string") {
+    targetName = ns.args[0];
+  }
+
+  /**
+   * The delay time before the operation starts.
+   */
+  var delay = 0;
+  if (ns.args.length > 1 && typeof ns.args[1] == "number") {
+    delay = ns.args[1];
+  }
+
+  /**
+   * The time at which grow started.
+   * @type {number}
+   */
+  var timeStampStart = ns.getTimeSinceLastAug();
+
+  if (delay > 0) {
+    await ns.sleep(delay);
+  }
+
+  await ns.grow(targetName);
+
+  /**
+   * The time at which grow finished.
+   * @type {number}
+   */
+  var timeStampEnd = ns.getTimeSinceLastAug();
+
+  /**
+   * The percentage of the maximum money currently on the target server.
+   * @type {number}
+   */
+  var money =
+    ns.getServerMoneyAvailable(targetName) / ns.getServerMaxMoney(targetName);
+
+  /**
+   * The difference between current security and minimum security on the target server.
+   * @type {number}
+   */
+  var security =
+    ns.getServerRequiredHackingLevel(targetName) -
+    ns.getServerMinSecurityLevel(targetName);
+
+  // print the result to the terminal
+  ns.tprint(
+    ns.sprintf(
+      "||Grow Finished | Money: %3.1f | Security: %3.1f | Time: %s ||",
+      money,
+      security,
+      ns.tFormat(timeStampEnd - timeStampStart, true)
+    )
+  );
+}
