02f0e380e7f1e82658ee50105337fad768a40895
mejand <andreas.mejstrik@gmx.at>
2021-12-29 15:55:44 +0100

    use target tiem instead of delay

--- hack.js
+++ hack.js
@@ -1,5 +1,5 @@
 /**
- * Run a single grow operation.
+ * Run a single hack operation and print the result for debugging.
  * @param {import(".").NS } ns
  */
 export async function main(ns) {
@@ -13,16 +13,103 @@ export async function main(ns) {
   }
 
   /**
-   * The delay time before the operation starts.
+   * The time at which the script shall finish its execution.
+   * @type {number}
    */
-  var delay = 0;
+  var targetTime = 0;
   if (ns.args.length > 1 && typeof ns.args[1] == "number") {
-    delay = ns.args[1];
+    targetTime = ns.args[1];
   }
 
-  if (delay > 0) {
-    await ns.sleep(delay);
+  /**
+   * The ID of the script instance.
+   * @type {number}
+   */
+  var id = 0;
+  if (ns.args.length > 2 && typeof ns.args[2] == "number") {
+    id = ns.args[2];
   }
 
-  await ns.hack(targetName);
+  /**
+   * The script is waiting for the right time to start its operation.
+   * @type {boolean}
+   */
+  var running = true;
+
+  /**
+   * The time at which script execution is predicted to finish.
+   * @type {number}
+   */
+  var predictedFinish = 0;
+
+  /**
+   * The current time.
+   * @type {number}
+   */
+  var timeNow = 0;
+
+  /**
+   * The time it takes for the main operation to finish.
+   * @type {number}
+   */
+  var runTime = getTimeInRaster(ns.getHackTime(targetName));
+
+  /**
+   * Keep looping until the execution start time has arrived
+   */
+  while (running) {
+    /**
+     * Update the current time stamp.
+     */
+    timeNow = ns.getTimeSinceLastAug();
+
+    /**
+     * Update the that the operation should finish if started now.
+     */
+    predictedFinish = timeNow + runTime;
+
+    /**
+     * Start the operation if it seems like the script will finish
+     * when commanded.
+     */
+    if (predictedFinish == targetTime) {
+      /**
+       * Ensure that the loop does not continue after the operation
+       * has finished.
+       */
+      running = false;
+
+      /**
+       * Start the main operation.
+       */
+      await ns.hack(targetName);
+    } else if (predictedFinish > targetTime) {
+      /**
+       * Stop the script if the correct time was missed.
+       */
+      running = false;
+    } else {
+      /**
+       * If the time is not right yet wait for the next 200ms step
+       */
+      await ns.sleep(200);
+    }
+  }
+}
+
+/**
+ * Convert a time in milliseconds to 200ms precision.
+ * @param {number} time - The time that shall be converted into 200ms steps.
+ * @returns {number} The input time converted into 200ms increments.
+ */
+function getTimeInRaster(time) {
+  /**
+   * Note: This method can cause the script to finish 200ms before
+   * the target time. This error seems to be random since the same
+   * execution time input can lead to both outcomes. Currently there
+   * appears to be no way around this and the accuracy of 400ms has to
+   * be accounted for in the controller script.
+   */
+
+  return Math.ceil(time / 200) * 200;
 }
