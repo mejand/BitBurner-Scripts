6fefe7698b789eac2c2033f0df367357a3e1981e
mejand <andreas.mejstrik@gmx.at>
2021-12-30 13:58:44 +0100

    Batch class

--- utilities.js
+++ utilities.js
@@ -14,3 +14,58 @@ export function getTimeInRaster(time) {
 
   return Math.ceil(time / 200) * 200;
 }
+
+/**
+ * The instructions needed to run a hack, grow, weaken cycle against a target server.
+ */
+export class Batch {
+  /**
+   * Create an instance of a batch.
+   * @param {string} targetName - The name of the target server.
+   */
+  constructor(targetName) {
+    /**
+     * The name of the target server.
+     * @type {string}
+     */
+    this.targetName = targetName;
+    /**
+     * The number of threads dedicated to hacking.
+     * @type {number}
+     */
+    this.hackThreads = 0;
+    /**
+     * The number of threads dedicated to growing.
+     * @type {number}
+     */
+    this.growThreads = 0;
+    /**
+     * The number of threads dedicated to weakening.
+     * @type {number}
+     */
+    this.weakenThreads = 0;
+    /**
+     * The time at which the hack operation shall be finished.
+     * @type {number}
+     */
+    this.hackFinish = 0;
+    /**
+     * The time at which the grow operation shall be finished.
+     * @type {number}
+     */
+    this.growFinish = 0;
+    /**
+     * The time at which the weaken operation shall be finished.
+     * @type {number}
+     */
+    this.weakenFinish = 0;
+  }
+
+  /**
+   * The total number of threads dedicated to this batch.
+   * @type {number}
+   */
+  get totalThreads() {
+    return this.hackThreads + this.growThreads + this.weakenThreads;
+  }
+}
