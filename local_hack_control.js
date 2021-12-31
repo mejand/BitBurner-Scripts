e41119076067359023a8345ff76cd74e79911f03
mejand <andreas.mejstrik@gmx.at>
2021-12-31 19:58:40 +0100

    reworked to use single batches

--- local_hack_control.js
+++ local_hack_control.js
@@ -1,8 +1,15 @@
+import {
+  logPrintVar,
+  getFarmingBatch,
+  getPreparationBatch,
+} from "./utilities.js";
+
 /**
  * Handle grow, weaken and hack scripts on the same host as the control script.
  * @param {import(".").NS } ns
  */
 export async function main(ns) {
+  ns.disableLog("ALL");
   /**
    * The name of the target server.
    * @type {string}
@@ -11,67 +18,100 @@ export async function main(ns) {
   if (ns.args.length > 0 && typeof (ns.args[0] == "string")) {
     targetName = ns.args[0];
   }
-
-  /**
-   * The amount of money under which a grow cycle will be started.
-   * @type {number}
-   */
-  var moneyThresh = ns.getServerMaxMoney(targetName) * 0.75;
-
-  /**
-   * The security level under which a weaken cycle will be started.
-   * @type {number}
-   */
-  var securityThresh = ns.getServerMinSecurityLevel(targetName) + 5;
-
   /**
    * The name of the host server the script runs on.
    * @type {string}
    */
   var hostName = ns.getHostname();
-
+  /**
+   * The server object of the target.
+   * @type {import(".").Server}
+   */
+  var target = ns.getServer(targetName);
+  /**
+   * The server object of the host the script is running on.
+   * @type {import(".").Server}
+   */
+  var host = ns.getServer(hostName);
   /**
    * The amount of ram available on the host server.
    * @type {number}
    */
-  var availableRam =
-    ns.getServerMaxRam(hostName) - ns.getServerUsedRam(hostName);
+  var availableRam = host.maxRam - host.ramUsed;
+  var hackThreads = 1;
+  var growThreads = 0;
+  var weakenThreads = 0;
+  var hackRam = ns.getScriptRam("dummyHack.js", hostName);
+  var growRam = ns.getScriptRam("dummyGrow.js", hostName);
+  var weakenRam = ns.getScriptRam("dummyWeaken.js", hostName);
+  var id = 0;
+  var sleepTime = 150;
 
-  // copy the needed scripts to the host
-  await ns.scp("weaken.js", "home", hostName);
-  await ns.scp("grow.js", "home", hostName);
-  await ns.scp("hack.js", "home", hostName);
+  ns.tail();
 
-  // run an infinate loop that keeps evaluating the status of the target whenever a script has finished
   while (true) {
-    // update the available ram on the host
-    availableRam = ns.getServerMaxRam(hostName) - ns.getServerUsedRam(hostName);
+    ns.clearLog();
+    /** Update the server objects */
+    target = ns.getServer(targetName);
+    host = ns.getServer(hostName);
 
-    // check which type of cycle needs to be started
-    if (ns.getServerSecurityLevel(targetName) > securityThresh) {
-      // If the server's security level is above our threshold, weaken it
-      let threadCount = Math.floor(availableRam / ns.getScriptRam("weaken.js"));
-      if (threadCount > 0) {
-        ns.run("weaken.js", threadCount, targetName);
-      }
-      await ns.sleep(ns.getWeakenTime(targetName));
-    } else if (ns.getServerMoneyAvailable(targetName) < moneyThresh) {
-      // If the server's money is less than our threshold, grow it
-      let threadCount = Math.floor(availableRam / ns.getScriptRam("grow.js"));
-      if (threadCount > 0) {
-        ns.run("grow.js", threadCount, targetName);
-      }
-      await ns.sleep(ns.getGrowTime(targetName));
+    sleepTime = 150;
+
+    logPrintVar(ns, "Money", (target.moneyAvailable / target.moneyMax) * 100);
+    logPrintVar(
+      ns,
+      "Delta Security",
+      target.hackDifficulty - target.minDifficulty
+    );
+
+    let ramAvailable = host.maxRam - host.ramUsed;
+    let batch = null;
+    if (
+      target.moneyAvailable < target.moneyMax * 0.8 ||
+      target.hackDifficulty - target.minDifficulty > 1
+    ) {
+      batch = getPreparationBatch(ns, target, host, ramAvailable / 2);
     } else {
-      // Otherwise, hack it
-      let threadCount = Math.floor(availableRam / ns.getScriptRam("hack.js"));
-      if (threadCount > 0) {
-        ns.run("hack.js", threadCount, targetName);
+      batch = getFarmingBatch(ns, target, host);
+    }
+    let ramNeeded =
+      batch.hackThreads * hackRam +
+      batch.growThreads +
+      growRam +
+      batch.weakenThreads * weakenRam;
+
+    logPrintVar(ns, "Hack Threads", batch.hackThreads);
+    logPrintVar(ns, "Grow Threads", batch.growThreads);
+    logPrintVar(ns, "Weaken Threads", batch.weakenThreads);
+    logPrintVar(ns, "RAM Available", ramAvailable);
+    logPrintVar(ns, "RAM Needed", ramNeeded);
+    logPrintVar(
+      ns,
+      "MoneyGain",
+      ns.getScriptIncome(ns.getScriptName(), host.hostname, ns.args[0])
+    );
+
+    if (ramNeeded < ramAvailable) {
+      if (batch.hackThreads > 0) {
+        ns.run("dummyHack.js", batch.hackThreads, target.hostname, id);
       }
-      await ns.sleep(ns.getHackTime(targetName));
+      if (batch.growThreads > 0) {
+        ns.run("dummyGrow.js", batch.growThreads, target.hostname, id);
+      }
+      if (batch.weakenThreads > 0) {
+        ns.run("dummyWeaken.js", batch.weakenThreads, target.hostname, id);
+      }
+
+      id++;
+
+      if (id > 10000) {
+        id = 0;
+      }
+
+      sleepTime = ns.getWeakenTime(target.hostname) + 400;
+      logPrintVar(ns, "Sleep Time", sleepTime);
     }
 
-    // await another 10ms to get some buffer time if there is a mismatch in the getXXXTime and sleep functions
-    await ns.sleep(10);
+    await ns.sleep(sleepTime);
   }
 }
