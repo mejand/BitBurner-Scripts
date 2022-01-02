f8d51a6b08dfcec24c8d4932e473bc41b9a1b522
mejand <andreas.mejstrik@gmx.at>
2022-01-02 13:21:04 +0100

    added hacknet management

--- start.js
+++ start.js
@@ -10,12 +10,17 @@ export async function main(ns) {
   var wait_time = 400;
   // call the spider script to populate the network map
   ns.run("/support/spider.js", 1);
-  ns.tprint("#### Network Mapped ####");
+  ns.tprint("####       Network Mapped       ####");
   await ns.sleep(wait_time);
 
   // start the unlock script with a 10 second period and debugging off
   ns.run("/support/unlock.js", 1, 10000);
-  ns.tprint("#### Server Unlocking Started ####");
+  ns.tprint("####  Server Unlocking Started  ####");
+  await ns.sleep(wait_time);
+
+  // start the hacknet script with a 1 second period and 50 budget
+  ns.run("/support/hacknet.js", 1, 1000, 0.5, false);
+  ns.tprint("#### Hacknet Management Started ####");
   await ns.sleep(wait_time);
 
   // start the centralized hacking control script
