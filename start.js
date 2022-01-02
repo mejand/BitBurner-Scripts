33117a1fc02781a04604757d4e731752a923a3df
mejand <andreas.mejstrik@gmx.at>
2022-01-02 13:08:10 +0100

    removed download from github

--- start.js
+++ start.js
@@ -4,23 +4,21 @@
  */
 export async function main(ns) {
   /**
-   * The url to the automated start up script.
-   * @type {string}
+   * The wait time between each step in the start up sequence.
+   * @type {number}
    */
-  var url =
-    "https://raw.githubusercontent.com/mejand/BitBurner-Scripts/main/automation_start.js";
+  var wait_time = 400;
+  // call the spider script to populate the network map
+  ns.run("/support/spider.js", 1);
+  ns.tprint("#### Network Mapped ####");
+  await ns.sleep(wait_time);
 
-  /**
-   * Download was successful.
-   * @type {boolean}
-   */
-  var success = await ns.wget(url, "automation_start.js", "home");
+  // start the unlock script with a 10 second period and debugging off
+  ns.run("/support/unlock.js", 1, 10000);
+  ns.tprint("#### Server Unlocking Started ####");
+  await ns.sleep(wait_time);
 
-  // inform the user about the success or failure and start the startup script
-  if (success) {
-    ns.tprint("#### Startup Script download successful ####");
-    ns.exec("automation_start.js", "home", 1, false);
-  } else {
-    ns.alert("#### Download of Startup Script failed ####");
-  }
+  // start the centralized hacking control script
+  ns.run("/controllers/singleBatch.js", 1);
+  ns.tprint("#### Local Hack Control Started ####");
 }
