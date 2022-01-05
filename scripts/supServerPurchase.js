37c9ff5ffef3e7e6a0589462d89989483efc12d8
mejand <andreas.mejstrik@gmx.at>
2022-01-05 18:41:42 +0100

    improved log window layout

--- scripts/supServerPurchase.js
+++ scripts/supServerPurchase.js
@@ -1,3 +1,5 @@
+import { logPrintVar, logPrintLine } from "./utilLog.js";
+
 /**
  * Handle the automatic buying of serversNames.
  * @param {import("..").NS} ns
@@ -11,7 +13,6 @@ export async function main(ns) {
   if (ns.args.length > 0 && typeof (ns.args[0] == "number")) {
     period = ns.args[0];
   }
-
   /**
    * The fraction of the player's money that can be spent on buying serversNames.
    * @type {number}
@@ -20,98 +21,83 @@ export async function main(ns) {
   if (ns.args.length > 1 && typeof (ns.args[1] == "number")) {
     maxMoney = ns.args[1];
   }
-
-  /**
-   * Enable debug logging.
-   * @type {boolean}
-   */
-  var debug = true;
-  if (ns.args.length > 2 && typeof (ns.args[2] == "boolean")) {
-    debug = ns.args[2];
-  }
-
   /**
    * The maximum money that can be spent on buying a new server.
    * @type {number}
    */
   var maxMoney = ns.getServerMoneyAvailable("home") * maxMoneyFactor;
-
   /**
    * The names of all currently purchased servers.
    * @type {string[]}
    */
   var serversNames = ns.getPurchasedServers();
-
   /**
    * The maximum amount of RAM that can be purchased.
    * @type {number}
    */
   var maxAffordableRam = 0;
-
   /**
    * The purchased server with the lowest RAM.
    * @type {import("..").Server}
    */
   var minRamServer = null;
-
   /**
    * The maximum number of servers that can be purchased.
    * @type {number}
    */
   var maxServerCount = ns.getPurchasedServerLimit();
 
-  // start the loop to periodically try to purchase a server
+  ns.disableLog("ALL");
+
+  /** Start the loop to periodically try to purchase a server */
   while (true) {
     // get the current player money
     maxMoney = ns.getServerMoneyAvailable("home") * maxMoneyFactor;
 
-    // get the maximum amount of RAM that could be purchased.
+    /** Get the maximum amount of RAM that could be purchased */
     maxAffordableRam = getMaxAffordableRam(ns, maxMoney);
 
-    // check if an existing server can be replaced by a new one
+    /** Check if an existing server can be replaced by a new one */
     if (serversNames.length == maxServerCount) {
-      // get the server with the maximum amount of RAM
+      /** Get the server with the maximum amount of RAM */
       minRamServer = getMinRamServer(ns, serversNames);
 
-      // check if a better server can be afforded
+      /** Check if a better server can be afforded */
       if (minRamServer.maxRam < maxAffordableRam) {
-        // delete the old server
+        /** Delete the old server */
         ns.killall(minRamServer.hostname);
         ns.deleteServer(minRamServer.hostname);
-        if (debug) {
-          ns.tprint(
-            minRamServer.hostname +
-              " at " +
-              minRamServer.maxRam +
-              "GB - deleted"
-          );
-        }
+        logPrintLine(ns);
+        logPrintVar(ns, "Deleted", minRamServer.hostname);
+        logPrintVar(ns, "RAM of Deleted", minRamServer.maxRam);
+        logPrintLine(ns);
       }
     }
 
-    // update the array of purchased servers
+    /** Update the array of purchased servers */
     serversNames = ns.getPurchasedServers();
 
-    // check if another server can be purchased
+    /** Check if another server can be purchased */
     if (serversNames.length < maxServerCount && maxAffordableRam > 0) {
       /**
        * The name of the new server that was purchased this cycle.
        * @type {string}
        */
-      var newServerName = ns.purchaseServer("owned-server", maxAffordableRam);
+      let newServerName = ns.purchaseServer("owned-server", maxAffordableRam);
 
-      // copy the hacking scripts to the new server
+      /** Copy the hacking scripts to the new server */
       await ns.scp("botsSingleGrow.js", "home", newServerName);
       await ns.scp("botsSingleHack.js", "home", newServerName);
       await ns.scp("botsSingleWeaken.js", "home", newServerName);
 
-      // print the name for debugging purposes
-      if (debug) {
-        ns.print("bought " + newServerName + " - " + maxAffordableRam + "GB");
-      }
+      /** Print the name for debugging purposes */
+      logPrintLine(ns);
+      logPrintVar(ns, "Bought", newServerName);
+      logPrintVar(ns, "RAM of Bought", maxAffordableRam);
+      logPrintLine(ns);
     }
 
-    // wait for the next execution cycle
+    /** Wait for the next execution cycle */
     await ns.sleep(period);
   }
 }
@@ -128,33 +114,29 @@ function getMaxAffordableRam(ns, money) {
    * @type {number}
    */
   var ram = 0;
-
   /**
    * The exponent for RAM calculation (2 ** i).
    * @type {number}
    */
   var i = 1;
-
   /**
    * Maximum RAM not reached -> continue the search.
    * @type {boolean}
    */
   var search = true;
-
   /**
    * The upper threshold for how much RAM can be purchased.
    * @type {number}
    */
   var maxRam = ns.getPurchasedServerMaxRam();
 
-  // iteratively increase the RAM amount until the price reaches the threshold.
+  /** Iteratively increase the RAM amount until the price reaches the threshold */
   while (search) {
     /**
      * The proposed RAM for examination of validity.
      * @type {number}
      */
     let proposedRam = 2 ** i;
-
     /**
      * The price for buying the proposed RAM.
      * @type {number}
@@ -162,11 +144,11 @@ function getMaxAffordableRam(ns, money) {
     let price = ns.getPurchasedServerCost(proposedRam);
 
     if (proposedRam <= maxRam && price <= money) {
-      // take over the proposal and continue the search
+      /** Take over the proposal and continue the search */
       ram = proposedRam;
       i++;
     } else {
-      // stop the search if the maximum RAM has been reached
+      /** Stop the search if the maximum RAM has been reached */
       search = false;
     }
   }
@@ -186,7 +168,6 @@ function getMinRamServer(ns, serversNames) {
    * @type {number}
    */
   var minRam = Infinity;
-
   /**
    * The name of the server with the lowest RAM.
    * @type {string}
