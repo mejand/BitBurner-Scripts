a6b7c55b7e4d8aabc381c82c71a631cac429ed31
mejand <andreas.mejstrik@gmx.at>
2022-01-05 13:50:20 +0100

    reworked folder structure for bitburner extension

---
+++ scripts/central_hack_control.js
@@ -0,0 +1,231 @@
+import { BatchHandler, Batch } from "./hack_distribution.js";
+
+/**
+ * Handle the growing, weakening and hacking scripts from one central server.
+ * @param {import("..").NS } ns
+ */
+export async function main(ns) {
+  /**
+   * Enable debug actions.
+   * @type {boolean}
+   */
+  var debug = true;
+  if (ns.args.length > 0 && typeof ns.args[0] == "boolean") {
+    debug = ns.args[0];
+  }
+
+  // clean up the log
+  ns.disableLog("ALL");
+
+  /**
+   * The RAM needed to run the hack script.
+   * @type {number}
+   */
+  var hackRam = ns.getScriptRam("hack.js", "home");
+
+  /**
+   * The RAM needed to run the grow script.
+   * @type {number}
+   */
+  var growRam = ns.getScriptRam("grow.js", "home");
+
+  /**
+   * The RAM needed to run the weaken script.
+   * @type {number}
+   */
+  var weakenRam = ns.getScriptRam("weaken.js", "home");
+
+  /**
+   * The amount of RAM needed to run any script.
+   * @type {number}
+   */
+  var ramPerScript = Math.max(hackRam, growRam, weakenRam);
+
+  /**
+   * The target server.
+   * @type {import("..").Server}
+   */
+  var targetServer = ns.getServer("n00dles");
+
+  /**
+   * The available host servers.
+   * @type {import("..").Server[]}
+   */
+  var hostServers = [];
+
+  /**
+   * The script handlers for each available host server.
+   * @type {BatchHandler[]}
+   */
+  var handlers = [];
+
+  /**
+   * The time between each calculation cycle.
+   * @type {number}
+   */
+  var cycleTime = 1000;
+
+  /**
+   * The running count of batches that have been created.
+   * @type {number}
+   */
+  var batchCount = 0;
+
+  /**
+   * The loading of all host servers in percent.
+   * @type {number}
+   */
+  var load = 0;
+
+  // run an infinate loop that keeps evaluating the status of the target whenever a script has finished
+  while (true) {
+    // clean up the log
+    ns.clearLog();
+
+    // reset values for the cycle
+    handlers = [];
+    batchCount = 0;
+    load = 0;
+
+    // update the target if debug mode is off
+    if (debug) {
+      // in debug mode the default target is used (get current server object)
+      targetServer = ns.getServer(targetServer.hostname);
+    } else {
+      // get the newest target (current copy of the server object will be provided by getTarget())
+      targetServer = getTarget(ns);
+    }
+
+    // update the available servers
+    hostServers = getAvailableServers(ns);
+
+    // loop through all host servers and create batch handlers for each one
+    for (let server of hostServers) {
+      handlers.push(
+        new BatchHandler(
+          ns,
+          targetServer.hostname,
+          server.hostname,
+          ramPerScript
+        )
+      );
+    }
+
+    // loop through all handlers to update and execute their batches
+    for (let handler of handlers) {
+      if (handler.hostServer.useable) {
+        handler.update(ns);
+        handler.execute(ns, batchCount);
+        batchCount += handler.batches.length;
+        load += handler.load;
+        ns.print(
+          handler.hostServer.name +
+            ": " +
+            ns.tFormat(
+              handler.batchTime + Batch.offsetPadding * handler.batches.length
+            ) +
+            " + " +
+            handler.hostServer.ramAvailable +
+            "GB " +
+            handler.batches.length +
+            " batches"
+        );
+      }
+    }
+
+    ns.print(
+      "load / handlers.length = " +
+        load +
+        " / " +
+        handlers.length +
+        " = " +
+        load / handlers.length
+    );
+
+    // scale the load value by the number of hosts
+    load = load / handlers.length;
+
+    ns.tprint(
+      ns.sprintf(
+        "||%s|Load: %3.1f|Money: %3.1f|Security: %3.1f|Batches: %i||",
+        targetServer.hostname,
+        load,
+        (targetServer.moneyAvailable / targetServer.moneyMax) * 100,
+        targetServer.hackDifficulty - targetServer.minDifficulty,
+        batchCount
+      )
+    );
+
+    // sleep while the scripts are active
+    await ns.sleep(cycleTime);
+  }
+}
+
+/**
+ * Get an array of the servers that are ready for tasking.
+ * @param {import("..").NS } ns
+ * @returns {import("..").Server[]} The server objects that are available for tasking.
+ */
+function getAvailableServers(ns) {
+  /**
+   * The names of all available servers
+   * @type {string[]}
+   */
+  var serverNames = ns.getPurchasedServers();
+
+  /**
+   * The rows of the network_unlocked.txt file.
+   * @type {string[]}
+   */
+  var rows = ns.read("network_unlocked.txt").split("\r\n");
+
+  // loop through all rows and add the names to the available servers
+  for (let row of rows) {
+    // Ignore last blank row
+    if (row) {
+      // get the server name and append it to the array
+      serverNames.push(row);
+    }
+  }
+
+  /**
+   * All available servers.
+   * @type {import("..").Server[]}
+   */
+  var servers = [];
+
+  // loop through all server names and get the corresponding server objects
+  for (let name of serverNames) {
+    servers.push(ns.getServer(name));
+  }
+
+  return servers;
+}
+
+/**
+ * Read the target from port 1 or provide a replacement.
+ * @param {import("..").NS } ns
+ * @returns {import("..").Server} The server object of the target.
+ */
+function getTarget(ns) {
+  /**
+   * The name of the target server.
+   * @type {string}
+   */
+  var targetName = ns.peek(1);
+
+  /**
+   * The target server object.
+   * @type {import("..").Server}
+   */
+  var target = null;
+
+  // check if the target name is valid and provide a replacement value if necessary
+  if (ns.serverExists(targetName)) {
+    target = ns.getServer(targetName);
+  } else {
+    target = ns.getServer("n00dles");
+  }
+
+  return target;
+}
---
+++ scripts/controllers/singleBatch.js
@@ -0,0 +1,128 @@
+import { logPrintVar } from "../utilities/log.js";
+import {
+  Batch,
+  getFarmingBatch,
+  getPreparationBatch,
+} from "../utilities/batch.js";
+import { MyServer } from "../utilities/server.js";
+import { getTarget } from "../utilities/com.js";
+
+/**
+ * Handle a single batch at a time on the local host server.
+ * @param {import("../..").NS } ns
+ */
+export async function main(ns) {
+  ns.disableLog("ALL");
+  /**
+   * The name of the host server the script runs on.
+   * @type {string}
+   */
+  var hostName = ns.getHostname();
+  /**
+   * The name of the hack script used in this controller.
+   * @type {string}
+   */
+  var hackScript = "/bots/singleHack.js";
+  /**
+   * The name of the grow script used in this controller.
+   * @type {string}
+   */
+  var growScript = "/bots/singleGrow.js";
+  /**
+   * The name of the hack script used in this controller.
+   * @type {string}
+   */
+  var weakenScript = "/bots/singleWeaken.js";
+  /**
+   * The RAM needed to run the hack script.
+   * @type {number}
+   */
+  var hackRam = ns.getScriptRam(hackScript, hostName);
+  /**
+   * The RAM needed to run the grow script.
+   * @type {number}
+   */
+  var growRam = ns.getScriptRam(growScript, hostName);
+  /**
+   * The RAM needed to run the weaken script.
+   * @type {number}
+   */
+  var weakenRam = ns.getScriptRam(weakenScript, hostName);
+  /**
+   * The RAM needed to run any of the bot scripts.
+   * @type {number}
+   */
+  var scriptRam = Math.max(hackRam, growRam, weakenRam);
+  /**
+   * The server object of the target.
+   * @type {MyServer}
+   */
+  var target = getTarget(ns);
+  /**
+   * The server object of the host the script is running on.
+   * @type {MyServer}
+   */
+  var host = new MyServer(ns, hostName, scriptRam);
+  /**
+   * The time in milliseconds the script shall wait before attempting
+   * to start the next batch.
+   * @type {number}
+   */
+  var sleepTime = 150;
+  /**
+   * The batch object that will be started by this cotnroler.
+   * @type {Batch}
+   */
+  var batch = null;
+
+  /** Open the log window */
+  ns.tail();
+
+  while (true) {
+    ns.clearLog();
+    /** Update the server objects */
+    target = getTarget(ns);
+    host.update(ns);
+
+    /** Reset the sleep time to ensure there are no unecessary wait times */
+    sleepTime = 150;
+    if (target) {
+      if (target.farming) {
+        batch = getFarmingBatch(ns, target.server, host.server);
+      } else {
+        batch = getPreparationBatch(
+          ns,
+          target.server,
+          host.server,
+          host.threadsAvailable
+        );
+      }
+
+      /** Start the batch if there are enough threads available */
+      if (batch.totalThreads <= host.threadsAvailable) {
+        if (batch.hackThreads > 0) {
+          ns.run(hackScript, batch.hackThreads, target.name);
+        }
+        if (batch.growThreads > 0) {
+          ns.run(growScript, batch.growThreads, target.name);
+        }
+        if (batch.weakenThreads > 0) {
+          ns.run(weakenScript, batch.weakenThreads, target.name);
+        }
+
+        /** Wait until the batch is finished to start the next patch.
+         * If no patch could be started the script will try again after 200ms.
+         */
+        sleepTime = ns.getWeakenTime(target.name) + 400;
+      }
+
+      /** Print information to the log window */
+      logPrintVar(ns, "Money on Target", target.moneyPercent);
+      logPrintVar(ns, "Load on Host", host.load);
+      logPrintVar(ns, "Sleep Time", sleepTime);
+      batch.print(ns);
+    }
+
+    await ns.sleep(sleepTime);
+  }
+}
