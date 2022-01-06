4e3be4df1290eee9436e62932ac4139daf1fa843
mejand <andreas.mejstrik@gmx.at>
2022-01-06 16:23:47 +0100

    player purchased servers are not included

--- scripts/supSpider.js
+++ scripts/supSpider.js
@@ -2,6 +2,7 @@ import { setNetworkMap } from "./utilCom.js";
 
 /**
  * Find all servers in the network and write them to a file for use in other scripts. Needs to be called only once per run.
+ * Servers purchased by the player are not included since they are not useful targets.
  * @param {import("..").NS } ns
  */
 export async function main(ns) {
@@ -26,9 +27,12 @@ export async function main(ns) {
 
     /** Loop through the connected servers and add any new servers */
     for (var j = 0; j < serversConnected.length; j++) {
-      /** If this server isn't in serversSeen, add it */
+      /** If this server isn't in the network map, add it */
       if (networkMap.indexOf(serversConnected[j]) === -1) {
-        networkMap.push(serversConnected[j]);
+        /** Ensure that purchased servers are not included */
+        if (!serversConnected[j].includes("owned-server")) {
+          networkMap.push(serversConnected[j]);
+        }
       }
     }
   }
