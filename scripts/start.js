f2e4abb39e01105efe099153f1b70beec8ccf8c6
mejand <andreas.mejstrik@gmx.at>
2022-01-06 16:02:33 +0100

    included target finding script

--- scripts/start.js
+++ scripts/start.js
@@ -9,29 +9,34 @@ export async function main(ns) {
    */
   var wait_time = 400;
 
-  // call the spider script to populate the network map
+  /** Call the spider script to populate the network map */
   ns.run("supSpider.js", 1);
   ns.tprint("####       Network Mapped       ####");
   await ns.sleep(wait_time);
 
-  // start the unlock script with a 10 second period
+  /** Start the unlock script with a 10 second period */
   ns.run("supUnlock.js", 1, 10000);
   ns.tprint("####  Server Unlocking Started  ####");
   await ns.sleep(wait_time);
 
-  // start the hacknet script with a 1 second period and 50% budget
+  /** Start the target finding script */
+  ns.run("supTarget.js", 1);
+  ns.tprint("####  Target Finding Started  ####");
+  await ns.sleep(wait_time);
+
+  /** Start the hacknet script with a 1 second period and 50% budget */
   ns.run("supHacknet.js", 1, 1000, 0.5);
   ns.tprint("#### Hacknet Management Started ####");
   await ns.sleep(wait_time);
 
   if (ns.getServerMaxRam("home") >= 128) {
-    // start the centralized hacking control script
+    /** Start the centralized hacking control script */
     ns.run("stckContLong.js", 1);
     ns.tprint("####   Stock Control Started    ####");
     await ns.sleep(wait_time);
   }
 
-  // start the centralized hacking control script
+  /** Start the centralized hacking control script */
   ns.run("ctrlSingleBatch.js", 1);
   ns.tprint("#### Local Hack Control Started ####");
 }
