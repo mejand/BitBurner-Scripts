69f760b3d5a64b8f76f6b73b2d2155ffebdf5b6d
mejand <andreas.mejstrik@gmx.at>
2022-01-07 13:01:50 +0100

    introduced total and remaining threads

--- scripts/utilBatch.js
+++ scripts/utilBatch.js
@@ -22,20 +22,35 @@ export class SingleBatch {
      */
     this.id = id;
     /**
-     * The number of threads dedicated to hacking.
+     * The number of threads dedicated to hacking that remain to be executed.
      * @type {number}
      */
-    this.hackThreads = 0;
+    this._hackThreadsRemaining = 0;
     /**
-     * The number of threads dedicated to growing.
+     * The number of threads dedicated to growing that remain to be executed.
      * @type {number}
      */
-    this.growThreads = 0;
+    this._growThreadsRemaining = 0;
     /**
-     * The number of threads dedicated to weakening.
+     * The number of threads dedicated to weakening that remain to be executed.
      * @type {number}
      */
-    this.weakenThreads = 0;
+    this._weakenThreadsRemaining = 0;
+    /**
+     * The total number of threads dedicated to hacking.
+     * @type {number}
+     */
+    this._hackThreadsTotal = 0;
+    /**
+     * The total number of threads dedicated to growing.
+     * @type {number}
+     */
+    this._growThreadsTotal = 0;
+    /**
+     * The total number of threads dedicated to weakening.
+     * @type {number}
+     */
+    this._weakenThreadsTotal = 0;
     /**
      * The name of the hack script.
      * @type {string}
@@ -69,25 +84,80 @@ export class SingleBatch {
   }
 
   /**
+   * The number of threads dedicated to hacking.
+   * @param {number} number - The total number of hack threads.
+   */
+  set hackThreads(number) {
+    this._hackThreadsTotal = number;
+    this._hackThreadsRemaining = number;
+  }
+  /**
+   * The number of threads dedicated to growing.
+   * @param {number} number - The total number of grow threads.
+   */
+  set growThreads(number) {
+    this._growThreadsTotal = number;
+    this._growThreadsRemaining = number;
+  }
+  /**
+   * The number of threads dedicated to growing.
+   * @param {number} number - The total number of weaken threads.
+   */
+  set weakenThreads(number) {
+    this._weakenThreadsTotal = number;
+    this._weakenThreadsRemaining = number;
+  }
+  get hackThreads() {
+    return this._hackThreadsTotal;
+  }
+  get growThreads() {
+    return this._growThreadsTotal;
+  }
+  get weakenThreads() {
+    return this._weakenThreadsTotal;
+  }
+  /**
    * The total number of threads dedicated to this batch.
    * @type {number}
    */
   get totalThreads() {
-    return this.hackThreads + this.growThreads + this.weakenThreads;
+    return (
+      this._hackThreadsTotal + this._growThreadsTotal + this._weakenThreadsTotal
+    );
   }
-
   /**
-   * The total of RAM needed to run this batch.
+   * The number of threads dedicated to this batch that remain to be executed.
+   * @type {number}
+   */
+  get remainingThreads() {
+    return (
+      this._hackThreadsRemaining +
+      this._growThreadsRemaining +
+      this._weakenThreadsRemaining
+    );
+  }
+  /**
+   * The total amount of RAM needed to execute this batch.
    * @type {number}
    */
   get totalRam() {
     return (
-      this.hackRam * this.hackThreads +
-      this.growRam * this.growThreads +
-      this.weakenRam * this.weakenThreads
+      this.hackRam * this._hackThreadsTotal +
+      this.growRam * this._growThreadsTotal +
+      this.weakenRam * this._weakenThreadsTotal
     );
   }
-
+  /**
+   * The remaining amount of RAM needed to finish executing this batch.
+   * @type {number}
+   */
+  get remainingRam() {
+    return (
+      this.hackRam * this._hackThreadsRemaining +
+      this.growRam * this._growThreadsRemaining +
+      this.weakenRam * this._weakenThreadsRemaining
+    );
+  }
   /**
    * Print a summary of the batch to the log.
    * @param {import("..").NS} ns
@@ -97,20 +167,19 @@ export class SingleBatch {
     text += "|         |    Hack    |    Grow    |   Weaken   |\n";
     text += ns.sprintf(
       "| Threads | %10i | %10i | %10i |\n",
-      this.hackThreads,
-      this.growThreads,
-      this.weakenThreads
+      this._hackThreadsTotal,
+      this._growThreadsTotal,
+      this._weakenThreadsTotal
     );
     text += ns.sprintf(
-      "| RAM     | %10i | %10i | %10i |\n",
-      this.hackRam * this.hackThreads,
-      this.growRam * this.growThreads,
-      this.weakenRam * this.weakenThreads
+      "| RAM     | %10.2f | %10.2f | %10.2f |\n",
+      this.hackRam * this._hackThreadsTotal,
+      this.growRam * this._growThreadsTotal,
+      this.weakenRam * this._weakenThreadsTotal
     );
     text += "+---------+------------+------------+------------+";
     ns.print(text);
   }
-
   /**
    * Execute the batch on the defined host servers.
    * @param {import("..").NS} ns
@@ -144,10 +213,10 @@ export class SingleBatch {
     var i = 0;
 
     /** Loop through host servers as long as there are threads left to start */
-    while (this.totalThreads > 0 && i < hosts.length) {
+    while (this.remainingThreads > 0 && i < hosts.length) {
       /** Calculate how many weaken threads can be started on this host */
       weakenThreadsActual = Math.min(
-        this.weakenThreads,
+        this._weakenThreadsRemaining,
         Math.floor(hosts[i].ramAvailable / this.weakenRam)
       );
       /** Start the weaken script if at least one thread can be executed */
@@ -164,13 +233,13 @@ export class SingleBatch {
          * if the script was started successfully.
          */
         if (result > 0) {
-          this.weakenThreads -= weakenThreadsActual;
+          this._weakenThreadsRemaining -= weakenThreadsActual;
           hosts[i].ramAvailable -= weakenThreadsActual * this.weakenRam;
         }
       }
       /** Calculate how many grow threads can be started on this host */
       growThreadsActual = Math.min(
-        this.growThreads,
+        this._growThreadsRemaining,
         Math.floor(hosts[i].ramAvailable / this.growRam)
       );
       /** Start the grow script if at least one thread can be executed */
@@ -187,13 +256,13 @@ export class SingleBatch {
          * if the script was started successfully.
          */
         if (result > 0) {
-          this.growThreads -= growThreadsActual;
+          this._growThreadsRemaining -= growThreadsActual;
           hosts[i].ramAvailable -= growThreadsActual * this.growRam;
         }
       }
       /** Calculate how many hack threads can be started on this host */
       hackThreadsActual = Math.min(
-        this.hackThreads,
+        this._hackThreadsRemaining,
         Math.floor(hosts[i].ramAvailable / this.hackRam)
       );
       /** Start the hack script if at least one thread can be executed */
@@ -210,7 +279,7 @@ export class SingleBatch {
          * if the script was started successfully.
          */
         if (result > 0) {
-          this.hackThreads -= hackThreadsActual;
+          this._hackThreadsRemaining -= hackThreadsActual;
           hosts[i].ramAvailable -= hackThreadsActual * this.hackRam;
         }
       }
@@ -218,7 +287,6 @@ export class SingleBatch {
       i++;
     }
   }
-
   /**
    * Scale the batch to use a maximum amount of RAM.
    * @param {number} ramAvailable - The maximum amount of RAM that shall be used by the batch.
