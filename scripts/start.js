69c70180317e7c777c53be44cccd0fd68d09b64f
mejand <andreas.mejstrik@gmx.at>
2022-01-09 15:30:29 +0100

    removed target script (redundnt at the moment)

--- scripts/start.js
+++ scripts/start.js
@@ -27,7 +27,6 @@ export async function main(ns) {
   var scripts = [
     "supSpider.js",
     "supUnlock.js",
-    "supTarget.js",
     "supHacknet.js",
     "supServerPurchase.js",
     "supCtrlStart.js",
