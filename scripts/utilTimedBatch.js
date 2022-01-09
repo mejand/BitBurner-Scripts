06f790483085b2ecbfe1e531ff6cc0398ca77aaa
mejand <andreas.mejstrik@gmx.at>
2022-01-09 19:32:13 +0100

    execute method for action

--- scripts/utilTimedBatch.js
+++ scripts/utilTimedBatch.js
@@ -13,14 +13,19 @@ class TimedAction {
   /**
    * Creat an instance of an action.
    * @param {import("..").NS} ns
-   * @param {string} script - The name of the script corresponding to the action.
+   * @param {number} type - The type of action (1 = hack, 2 = grow, 3 = weaken).
    */
-  constructor(ns, script) {
+  constructor(ns, type) {
+    /**
+     * The type of action (1 = hack, 2 = grow, 3 = weaken).
+     * @type {number}
+     */
+    this._type = type;
     /**
      * The name of the script that corresponds to the action.
      * @type {string}
      */
-    this._script = script;
+    this._script = "botsTimedSelect.js";
     /**
      * The amount of RAM needed to execute this action with one thread.
      * @type {number}
@@ -41,13 +46,10 @@ class TimedAction {
      * @type {number}
      */
     this._threadsRemaining = 0;
-  }
-  /**
-   * The name of the script that corresponds to the action.
-   * @type {string}
-   */
-  get script() {
-    return this._script;
+
+    /** Limit the type to the permitted range */
+    this._type = Math.min(3, this._type);
+    this._type = Math.max(1, this._type);
   }
   /**
    * The amount of RAM needed to execute this action.
@@ -86,5 +88,64 @@ class TimedAction {
   set threadsRemaining(number) {
     this._ram = this._ramPerThread * number;
     this._threadsRemaining = number;
+  }
+  /**
+   * Execute the action on the given hosts.
+   * @param {import("..").NS} ns
+   * @param {string} targetName - The name of the target server.
+   * @param {number} finishTime - The time at which the action shall be finished.
+   * @param {number} id - The id of the batch this action is a part of.
+   * @param {import("..").Server[]} hosts - The available host servers.
+   */
+  execute(ns, targetName, finishTime, id, hosts) {
+    /**
+     * Index of the host server that is currently being attempted.
+     * @type {number}
+     */
+    var i = 0;
+    /**
+     * The amount of RAM left on the host server.
+     * @type {number}
+     */
+    var ramAvaialble = 0;
+    /**
+     * The number of threads that can be started on the host.
+     * @type {number}
+     */
+    let threads = 0;
+
+    /** Loop as long until there are no more threads or hosts remaning */
+    while (this.threadsRemaining > 0 && i < hosts.length) {
+      /** Analyze the current host */
+      ramAvaialble = hosts[i].maxRam - hosts[i].ramUsed;
+      threads = Math.min(
+        this.threadsRemaining,
+        Math.floor(ramAvaialble / this._ramPerThread)
+      );
+
+      /**
+       * The ID of the script that was just started. Will be 0 if the
+       * script could not be stared.
+       * @type {number}
+       */
+      let processId = ns.exec(
+        this._script,
+        hosts[i].hostname,
+        threads,
+        targetName,
+        finishTime,
+        id,
+        this._type
+      );
+
+      /** Update the status of the host server and the reamining threads */
+      if (processId > 0) {
+        hosts[i].ramUsed += threads * this._ramPerThread;
+        this.threadsRemaining -= threads;
+      }
+
+      /** Increment the counter to look at the next host */
+      i++;
+    }
   }
 }
