a4ee09736bfea2e3e8a6c66c846394099b721b5f
mejand <andreas.mejstrik@gmx.at>
2022-01-10 18:51:14 +0100

    state machine implementation

--- scripts/ctrlTimedBatch.js
+++ scripts/ctrlTimedBatch.js
@@ -1,4 +1,8 @@
-import { TimedBatch, getTimedFarmingBatch } from "./utilTimedBatch.js";
+import {
+  TimedBatch,
+  getTimedFarmingBatch,
+  getTimedPreparationBatch,
+} from "./utilTimedBatch.js";
 
 /**
  * Handle a single batch at a time on the local host server.
@@ -11,9 +15,9 @@ export async function main(ns) {
    * without a target.
    * @type {string}
    */
-  var targetName = null;
+  var target = null;
   if (ns.args.length > 0 && typeof (ns.args[0] == "string")) {
-    targetName = ns.args[0];
+    target = ns.args[0];
   }
   /**
    * The time in milliseconds that shall pass between batch executions.
@@ -56,35 +60,105 @@ export async function main(ns) {
    * @type {String[]}
    */
   var hosts = [];
-
   /**
-   * Update the number of batches that can be executed:
-   * The last batch has to start it's hack action before the hack action
-   * of the first one finishes so that the security on the target server
-   * is still at minimum.
+   * The current state of the script (1 = Waiting, 2 = Preparation, 3 = Farming).
+   * @type {Number}
    */
-  hackTime = ns.getHackTime(targetName);
-  batchCount = Math.floor(hackTime / period);
-  batchCountRemaining = batchCount;
+  var state = 1;
+  /**
+   * A string describing the current state.
+   * @type {Named}
+   */
+  var stateName = "Waiting";
+  /**
+   * The time until which the script shall wait before moving to the next state.
+   * @type {Number}
+   */
+  var waitUntil = 0;
 
   ns.tail();
 
   while (batchCountRemaining > 0) {
+    /** Get the current time */
     now = ns.getTimeSinceLastAug();
 
-    if (now % period == 0) {
-      batch = getTimedFarmingBatch(ns, targetName, id);
-      hosts = ns.getPurchasedServers();
-      batch.execute(ns, hosts);
-      batchCountRemaining--;
-      id++;
+    switch (state) {
+      case 1:
+        /** ------------- State 1 = Waiting ------------- */
+        if (now >= waitUntil) {
+          /** Decide if the target should be prepared or grown */
+          let money =
+            ns.getServerMoneyAvailable(target) / ns.getServerMaxMoney(target);
+          let security =
+            ns.getServerSecurityLevel(target) -
+            ns.getServerMinSecurityLevel(target);
+
+          if (money > 0.9 && security < 1) {
+            state = 3;
+            stateName = "Farming";
+            waitUntil = 0;
+          } else {
+            state = 2;
+            stateName = "Preparation";
+            waitUntil = 0;
+          }
+        }
+        break;
+      case 2:
+        /** ------------- State 1 = Preparation --------- */
+        if (now % period == 0) {
+          /** Start the preparation batch */
+          batch = getTimedPreparationBatch(ns, target, id);
+          hosts = ns.getPurchasedServers();
+          waitUntil = batch.execute(ns, hosts);
+          id++;
+          /** Move to the waiting state to re-evalute once its done */
+          state = 1;
+          stateName = "Waiting";
+          batch = null;
+          hosts = null;
+        }
+        break;
+      case 3:
+        /** ------------- State 3 = Farming ------------- */
+        /** Start new batches until there are none remaining */
+        if (now % period == 0) {
+          if (hackTime) {
+            batch = getTimedFarmingBatch(ns, target, id);
+            hosts = ns.getPurchasedServers();
+            waitUntil = batch.execute(ns, hosts);
+            batchCountRemaining--;
+            id++;
+          } else {
+            /**
+             * Update the number of batches that can be executed:
+             * The last batch has to start it's hack action before the hack action
+             * of the first one finishes so that the security on the target server
+             * is still at minimum.
+             */
+            hackTime = ns.getHackTime(target);
+            batchCount = Math.floor(hackTime / period);
+            batchCountRemaining = batchCount;
+          }
+        }
+        /** Move to the waiting state after all batches have been started */
+        if (batchCountRemaining == 0) {
+          state = 1;
+          stateName = "Waiting";
+          /** Reset the batch information to prepare for the next run */
+          batch = null;
+          hackTime = null;
+          batchCount = null;
+          batchCountRemaining = null;
+        }
+        break;
     }
 
     ns.clearLog();
+    ns.print("State = " + stateName);
     ns.print("hackTime = " + hackTime);
     ns.print("batchCount = " + batchCount);
     ns.print("batchCountRemaining = " + batchCountRemaining);
-
     await ns.sleep(150);
   }
 }
